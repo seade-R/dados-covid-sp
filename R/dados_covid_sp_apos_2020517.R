46df67bdecd0ccf91b3c7e37bd3c365fe139ab73
leobarone <leobarone@gmail.com>
2020-07-01 11:12:54 -0300

    20200701

--- R/dados_covid_sp_apos_2020517.R
+++ R/dados_covid_sp_apos_2020517.R
@@ -36,6 +36,7 @@ df2 <- excel_sheets(arquivo_xlsx) %>%
              mes = replace(mes, mes == 'abr', 4),
              mes = replace(mes, mes == 'mai', 5),
              mes = replace(mes, mes == 'jun', 6),
+             mes = replace(mes, mes == 'jul', 7),
              mes = as.numeric(mes)) %>% 
       select(-dia_mes)
     
